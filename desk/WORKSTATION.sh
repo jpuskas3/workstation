8c99e4f8bd840e48c99c9c53fd2e6b449e46d1e2
jpuskas3 <160540897+jpuskas3@users.noreply.github.com>
2024-11-18 21:52:47 -0500

    Update WORKSTATION.sh

--- desk/WORKSTATION.sh
+++ desk/WORKSTATION.sh
@@ -2,5 +2,5 @@
 cd
 source venv/bin/activate
 cd workstation
-konsole -e --qwindowgeometry 1038x300+50+50 "./intro.sh" & python3 scraper_gui.py & python3 app_gui.py & chromium webstation/webstation.html
+konsole -e --qwindowgeometry 1038x300+50+50 "./intro.sh" & python3 scraper_gui.py & python3 app_gui.py & chromium webstation.html
 
